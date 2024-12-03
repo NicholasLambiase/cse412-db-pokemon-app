f1a6f0d9f9e18640e8daef96d905e21fac3bc5a1
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-12-03 09:26:15 -0700

    matched schema and csv columns

--- backend/Pokemon-DB-Schema.sql
+++ backend/Pokemon-DB-Schema.sql
@@ -1,17 +1,18 @@
 -- Pokemon table schema
 CREATE TABLE Pokemon (
-	NAME VARCHAR(100) PRIMARY KEY,
+	NAME VARCHAR(30) PRIMARY KEY,
 	height DECIMAL(4,2) NULL,
 	weight DECIMAL(5,2) NULL,
-	type1 VARCHAR(200),
+	type1 VARCHAR(25),
+	type2 VARCHAR(25),
 	pokedex_number INT,
 	generation INT
 );
 
 -- Abilities table schema with foreign key reference to Pokemon and ON DELETE CASCADE
 CREATE TABLE Abilities (
-	NAME VARCHAR(100) PRIMARY KEY,
-	abilities VARCHAR(300),
+	NAME VARCHAR(30) PRIMARY KEY,
+	abilities VARCHAR(100),
 	CONSTRAINT fk_pokemon_name_abilities
 		FOREIGN KEY (NAME) REFERENCES Pokemon(NAME)
 		ON DELETE CASCADE
@@ -19,7 +20,7 @@ CREATE TABLE Abilities (
 
 -- Stats table schema with foreign key reference to Pokemon and ON DELETE CASCADE
 CREATE TABLE Stats (
-	NAME VARCHAR(100) PRIMARY KEY,
+	NAME VARCHAR(30) PRIMARY KEY,
 	attack INT,
 	special_attack INT,
 	speed INT,
